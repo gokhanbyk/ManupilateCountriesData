4f3b07225f1d651f28f6383d75af2825d020d759
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-11-12 11:34:55 +0300

    javascript üzerinden etiket oluşturma

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ input.setAttribute('placeholder', 'Değerim')
 // console.log(input.getAttribute('placeholder'))
 //? Attribute'u silme
 // input.removeAttribute('placeholder')
+
+
+//! JavaScriptten etiket oluşturma
+const div = document.createElement('div')
+div.style.width = "300px"
+div.style.height = "300px"
+div.style.border = "1px solid black"
+
+//* oluşturulan etiketi body'e gönderme
+document.body.appendChild(div)
+//* oluşturulan etiketi htmlden kaldırma
+document.body.removeChild(div)
+//! JavaScriptten etiket oluşturma
