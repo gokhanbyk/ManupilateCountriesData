9700e39dc433ec7d3b98f5816fef24271ddb6d65
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-11-12 11:01:50 +0300

    html sayfama input ekledim js ile placeholderi ekledik

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ for (let i = 0; i < countries.length; i++) {
     console.log(countries[i]['name'])
 }
 
+const input = document.getElementById('input')
+//? Attribute Ekleme
+input.setAttribute('placeholder', 'Değerim')
+//? Attribute içindeki bilgiyi getirme
+// console.log(input.getAttribute('placeholder'))
+//? Attribute'u silme
+// input.removeAttribute('placeholder')
